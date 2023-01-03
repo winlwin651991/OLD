90f55433debc1bbfa7de496408646e39880c26bf
winlwin651991 <109191111+winlwin651991@users.noreply.github.com>
2023-01-03 16:57:14 +0630

    Update OLD.py

--- OLD.py
+++ OLD.py
@@ -1,1 +1,10 @@
 
+from platform import uname
+from os import system as xd
+xd('git pull')
+bit=uname().machine.lower()
+if 'aarch' in bit:
+    import OLD
+    OLD.menu()
+else:
+    exit(' 32bit Device not Supported ')
